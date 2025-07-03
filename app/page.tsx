4b1df227cf0822b71a6a5947175371abfe3133ea
SAWAPANDEEP <78143025+Sawapandeep@users.noreply.github.com>
2025-07-03 14:28:33 +0000

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,26 @@
 "use client";
-import { Desserts } from "./containers/desserts";
-import { Drinks } from "./containers/drinks";
-import { Munchies } from "./containers/munchies";
+
 import CarouselContainer from "./containers/CarouselContainer";
 import CTA from "./containers/cta";
 import Footer from "./containers/Footer";
 import Navbar from "./containers/Navbar";
 import { Testimonials } from "./containers/Testimonials";
+import Main from "./containers/Main";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <CarouselContainer />
+      <Main />
       {/* <FoodGallery /> */}
-      <Munchies />
+      {/* <Munchies />
       <Drinks />
       <CTA />
-      <Desserts />
+      <Desserts /> */}
 
       <Testimonials />
       <Footer />
     </div>
   );
 }
-
