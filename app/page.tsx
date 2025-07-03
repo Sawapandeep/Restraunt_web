ccc1f95faef7bbae2406b7f8681bcfc7655eba4f
SAWAPANDEEP <78143025+Sawapandeep@users.noreply.github.com>
2025-07-03 13:52:43 +0000

    addition of containers_1

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,28 @@
 "use client";
-import { Desserts } from "./containers/desserts";
-import { Drinks } from "./containers/drinks";
-import { Munchies } from "./containers/munchies";
+
 import CarouselContainer from "./containers/CarouselContainer";
-import CTA from "./containers/cta";
+
 import Footer from "./containers/Footer";
 import Navbar from "./containers/Navbar";
 import { Testimonials } from "./containers/Testimonials";
+import Main from "./containers/Main";
+import AboutUs from "./containers/AboutUs";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <CarouselContainer />
+      <Main />
       {/* <FoodGallery /> */}
-      <Munchies />
+      {/* <Munchies />
       <Drinks />
       <CTA />
-      <Desserts />
+      <Desserts /> */}
 
       <Testimonials />
+      <AboutUs />
       <Footer />
     </div>
   );
 }
-
