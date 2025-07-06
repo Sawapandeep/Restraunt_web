9a82a9a265fb0170deebcbb12fdbc1df11a590cf
SAWAPANDEEP <78143025+Sawapandeep@users.noreply.github.com>
2025-07-06 15:36:49 +0530

    changes

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -15,3 +15,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
+
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,25 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#a855f7",
+          secondary: "#7e22ce",
+          accent: "#38bdf8",
+          neutral: "#d8b4fe",
+          "base-100": "#1c1917",
+          info: "#38bdf8",
+          success: "#4ade80",
+          warning: "#fde047",
+          error: "#ff0000",
+        },
+      },
+    ],
+  },
+ 
 };
 
 export default config;
